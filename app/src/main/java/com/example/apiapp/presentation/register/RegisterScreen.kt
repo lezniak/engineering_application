9422657c8639afa0cea8328e9bd9a29dbd297a31
Daniel <ddezersl@gmail.com>
2022-10-12 19:13:49 +0200

    3 steep register screens

--- app/src/main/java/com/example/apiapp/presentation/register/RegisterScreen.kt
+++ app/src/main/java/com/example/apiapp/presentation/register/RegisterScreen.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,21 +26,24 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.apiapp.R
 import com.example.apiapp.common.MyButton
+import com.squaredem.composecalendar.ComposeCalendar
+import java.time.LocalDate
 
 @Composable
 fun RegisterScreen(navHostController: NavHostController) {
-    var stepFlag by rememberSaveable() { mutableStateOf(false) }
+    var stepFlag by rememberSaveable() { mutableStateOf(0) }
     BackButton(navHostController)
     Column(modifier = Modifier
         .padding(40.dp)
         .fillMaxSize(),verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)) {
-        if (!stepFlag){
-            StepOneScreen()
-        }else{
-            StepTwoScreen()
+
+        when(stepFlag){
+            0 -> StepOneScreen()
+            1 -> StepTwoScreen()
+            2 -> StepThreeScreen()
         }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
-            MyButton(onClick = { stepFlag = !stepFlag}) {
+            MyButton(onClick = { stepFlag++ }) {
                 Text(text = "Dalej", color = Color.White)
             }
         }
@@ -48,18 +51,52 @@ fun RegisterScreen(navHostController: NavHostController) {
 
 }
 @Composable
-fun StepTwoScreen(){
-    Text(text = "Dalej")
+fun StepThreeScreen(){
+    WelcomeText(header = "Ostatni krok!", body = "Aby zakonczyć rejestracje podaj ")
 }
 @Composable
 fun StepOneScreen(){
-    WelcomeText()
+    WelcomeText(header = "Krok pierwszy", body = "Wprowadź swoje imię, abyśmy wiedzieli jak mamy do Ciebie sie zwracać! No i oczywiście ile masz lat.")
+    var name by rememberSaveable { mutableStateOf("") }
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Imie") },
+            singleLine = true,
+            leadingIcon = { Icon(Icons.Filled.AccountBox, contentDescription = "Email") },
+        )
+
+// first declare a variable that holds the dialog visibility state
+        val showDialog = rememberSaveable { mutableStateOf(false) }
+
+// then show the dialog based on the state
+        if (showDialog.value) {
+            ComposeCalendar(
+                onDone = { it: LocalDate ->
+                    // Hide dialog
+                    showDialog.value = false
+                    // Do something with the date
+                },
+                onDismiss = {
+                    // Hide dialog
+                    showDialog.value = false
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun StepTwoScreen(){
+    WelcomeText("Krok drugi","Aby przejść do następnego kroku wypełnij poprawnie poniższe pola.\"")
     PasswordEmailInputs()
 }
 @Composable
-fun WelcomeText(){
-    Text(text = "Krok pierwszy", fontWeight = FontWeight.Bold, fontSize = 26.sp)
-    Text(text = "Aby przejść do następnego kroku wypełnij poprawnie poniższe pola.",color = Color(128,128,128), fontSize = 12.sp )
+fun WelcomeText(header : String,body: String){
+    Text(text = header, fontWeight = FontWeight.Bold, fontSize = 26.sp)
+    Text(text = body,color = Color(128,128,128), fontSize = 12.sp )
 }
 @Composable
 fun PasswordEmailInputs() {
---
+++ app/src/main/java/com/example/apiapp/presentation/register/RegisterViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.apiapp.presentation.register
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor() : ViewModel() {
+
+}
