06fa013c45c0818daed40c5c07017ffb8c16ecb5
Daniel <ddezersl@gmail.com>
2022-11-09 15:46:44 +0100

    event detail parametr

--- app/src/main/java/com/example/apiapp/presentation/afterLogin/events/details/EventDetail.kt
+++ app/src/main/java/com/example/apiapp/presentation/afterLogin/events/details/EventDetail.kt
@@ -2,8 +2,9 @@ package com.example.apiapp.presentation.afterLogin.events.details
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import com.example.apiapp.data.objects.Event
 
 @Composable
-fun EventDetail(navHostController: NavHostController) {
+fun EventDetail(navHostController: NavHostController,event : Event) {
 
 }
